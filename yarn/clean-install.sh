38e6d11d9e31c42aa7798850abaf74caa1e3431a
JonathanDsouza6 <jonathan.d.souza@ah.nl>
2025-02-27 09:11:04 +0100

    Update yarn cache cleaning to use --mirror option for improved performance

--- yarn/clean-install.sh
+++ yarn/clean-install.sh
@@ -13,7 +13,7 @@ check_yarn() {
 # Clear yarn cache system wide
 clean_cache() {
     echo "Clearing system-wide yarn cache..."
-    yarn cache clean
+    yarn cache clean --mirror
 
     # Clear yarn cache in current folder
     echo "Clearing local yarn cache..."
